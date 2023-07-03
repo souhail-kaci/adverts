488893dc7b47329c6743b14915c75b17275783bb
souhail-kaci <souhail_196@hotmail.com>
2023-07-03 22:10:10 +0200

    add spring configuration

---
+++ src/main/java/fr/souhail/adverts/configuration/AppConfiguration.java
@@ -0,0 +1,26 @@
+package fr.souhail.adverts.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "fr.souhail.adverts")
+public class AppConfiguration {
+
+
+    @Bean
+    public ViewResolver viewResolver() {
+
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+
+}
