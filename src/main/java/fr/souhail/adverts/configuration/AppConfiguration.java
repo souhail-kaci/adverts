f4412df6a72006e80b4dd56d0c905374ae47165e
souhail-kaci <souhail_196@hotmail.com>
2023-07-03 23:54:58 +0200

    add custom login page + add bootstrap

--- src/main/java/fr/souhail/adverts/configuration/AppConfiguration.java
+++ src/main/java/fr/souhail/adverts/configuration/AppConfiguration.java
@@ -6,13 +6,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "fr.souhail.adverts")
-public class AppConfiguration {
+public class AppConfiguration implements WebMvcConfigurer {
 
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
+
+    }
 
     @Bean
     public ViewResolver viewResolver() {
--- src/main/java/fr/souhail/adverts/configuration/hibernate/HibernateConfiguration.java
+++ src/main/java/fr/souhail/adverts/configuration/hibernate/HibernateConfiguration.java
@@ -81,7 +81,7 @@ public class HibernateConfiguration {
 
     Properties additionalProperties() {
         Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", "create");
+        properties.setProperty("hibernate.hbm2ddl.auto", "update");
         properties.setProperty("hibernate.show_sql", "true");
         properties.setProperty("hibernate.format_sql", "true");
         properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
